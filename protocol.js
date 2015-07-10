22b92a067af0eec7c98cc6fecaa473d6b4ef6126
Joshua Melville <joshmelville@gmail.com>
2015-07-10 14:06:24 +0100

    setup

---
+++ protocol.js
@@ -0,0 +1,11 @@
+var protocol = {
+    sessionParameters: {
+        name: "Dhil Protocol"
+    },
+    skipFunctions: {
+        exampleSkip: function() { return false; }
+    },
+    stages: [
+        {label:'Example',page:'intro.html'}
+    ]
+};
