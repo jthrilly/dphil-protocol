95ebd35b821c66a1e9f77a17a3a9d4bb359c219b
Joshua Melville <joshmelville@gmail.com>
2016-01-26 00:43:01 +0000

    implement narrative interface

--- protocol.js
+++ protocol.js
@@ -201,13 +201,13 @@ var protocol = {
         {icon: 'fa-file-text', label:'NG: Advice', page:'namegen-advice.html'},
         {icon: 'fa-file-text', label:'NG: Information', page:'namegen-information.html'},
         {icon: 'fa-file-text', label:'NG: Technology', page:'namegen-technology.html'},
-        {icon: 'fa-file-text', label:'Layout', page:'layout.html'},
-        {icon: 'fa-file-text', label:'Sociogram Missing', page:'sociogram-missing.html'},
-        {icon: 'fa-file-text', label:'Sociogram Multi', page:'sociogram-multi.html'},
+        {icon: 'fa-bullseye', label:'Layout', page:'layout.html'},
+        {icon: 'fa-bullseye', label:'Sociogram Missing', page:'sociogram-missing.html'},
+        {icon: 'fa-bullseye', label:'Sociogram Multi', page:'sociogram-multi.html'},
         {icon: 'fa-file-text', label:'Ord: Helpfulness', page:'ord-helpfulness.html'},
-        {icon: 'fa-file-text', label:'Ord: Helpfulness', page:'ord-disempowering.html'},
-        {icon: 'fa-file-text', label:'Ord: Helpfulness', page:'ord-stress.html'},
-        {icon: 'fa-file-text', label:'Narrative', page:'sociogram-narrative.html'},
+        {icon: 'fa-file-text', label:'Ord: Disempowering', page:'ord-disempowering.html'},
+        {icon: 'fa-file-text', label:'Ord: Stress', page:'ord-stress.html'},
+        {icon: 'fa-bullseye', label:'Narrative', page:'sociogram-narrative.html'},
 
     ]
 };
